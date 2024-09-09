4ad24c3dba97dc0bbf45f483e7071ca14d9b9ca1
ashishpatel00011 <iamanshu.7987@gmail.com>
2024-09-09 11:18:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "dist", // Make sure the build output is in the "dist" folder
+  },
+  base: "/", // Set base path correctly for Vercel deployment
+  preview: {
+    port: 8080, // Port for Vite preview
+  },
+});
